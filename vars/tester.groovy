823f3578b82afd6e6376d2e4c51213827009a4ce
Sandu Postaru <sandu.postaru@thalesgroup.com>
2020-02-11 16:26:58 +0100

    Update tester

--- vars/tester.groovy
+++ vars/tester.groovy
@@ -1,11 +1,4 @@
-private def getRunnerCmd(String capellaProductPath) {
-  return "${capellaProductPath} " +
-			"-port 8081 " +
-			"-application org.polarsys.capella.test.run.application " +
-			"-data ${WORKSPACE}/runner >> ${WORKSPACE}/runner.log"
-}
-
-private def getJunitCmdTemplate(String capellaProductPath) {
+private def getJunitCmdTemplate(String capellaProductPath, String applicationParam) {
   
   // extract the capella path, without the executable name
   def capellaPath = capellaProductPath.substring(0, capellaProductPath.lastIndexOf("/"))
@@ -23,19 +16,20 @@ private def getJunitCmdTemplate(String capellaProductPath) {
       "-port 8081 " +
       "-testLoaderClass org.eclipse.jdt.internal.junit4.runner.JUnit4TestLoader " +
       "-loaderpluginname org.eclipse.jdt.junit4.runtime " +
+      "-application ${applicationParam} " +
       "-product org.polarsys.capella.rcp.product " +
       "-testApplication org.polarsys.capella.core.platform.sirius.ui.perspective.id " +
-      "-configuration file:${capellaPath}/configuration " +
+      "-configuration file:${env.WORKSPACE}/capella/configuration " +
       "-buildKey ${BUILD_KEY}"
 }
 
 
 private def getUIJunitCmd(String capellaProductPath) {
-	return getJunitCmdTemplate(capellaProductPath) + " -application org.eclipse.pde.junit.runtime.uitestapplication"
+	return getJunitCmdTemplate(capellaProductPath, 'org.eclipse.pde.junit.runtime.uitestapplication')
 }
 
 private def getNONUIJunitCmd(String capellaProductPath) {
-  return getJunitCmdTemplate(capellaProductPath) + " -application org.eclipse.pde.junit.runtime.nonuithreadtestapplication"
+  return getJunitCmdTemplate(capellaProductPath, 'org.eclipse.pde.junit.runtime.nonuithreadtestapplication')
 }
 
 def runUITests(String capellaProductPath, String suiteTitle, String testPluginName, List<String> testClassNames) {

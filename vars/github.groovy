2a426ebd8aeeb7aed96b2f1123b16a98351a6cc7
Sandu Postaru <sandu.postaru@thalesgroup.com>
2020-02-11 10:40:10 +0100

    Remove newlines

--- vars/github.groovy
+++ vars/github.groovy
@@ -2,13 +2,7 @@ def pullRequestComment(pullRequestId, message) {
   def apiURL = "https://api.github.com/repos/eclipse/capella/issues/${pullRequestId}/comments"
   
   withCredentials([string(credentialsId: '0dea5761-867c-44db-92fa-9304c81a8653', variable: 'password')]) {
-  	sh """
-    	curl ${apiURL}
-      	-d '{"body":"${message}"}' 
-      	-u "eclipse-capella-bot:${password}" 
-				-X POST 
-				-H "Content-Type: application/json"
-  	"""
+  	sh """curl ${apiURL} -d '{"body":"${message}"}' -u "eclipse-capella-bot:${password}" -X POST -H "Content-Type: application/json""""
   }
 }
 
@@ -32,6 +26,12 @@ def buildFailedComment() {
 
 def buildUnstableComment(String pullRequestId) {
   def message = ":worried: Build [${CHANGE_ID}-${BUILD_ID}](${BUILD_URL}) is unstable! The product is available [here](http://download.eclipse.org/capella/core/products/nightly/${BUILD_KEY})."
+  
+  pullRequestComment(CHANGE_ID, message)
+}
+
+def buildAbortedComment(String pullRequestId) {
+  def message = ":neutral_face: Build [${CHANGE_ID}-${BUILD_ID}](${BUILD_URL}) has been aborted!"
   
   pullRequestComment(CHANGE_ID, message)
 }
--- vars/tester.groovy
+++ vars/tester.groovy
@@ -1,10 +1,9 @@
 private def getRunnerCmd(String capellaProductPath) {
-  return """
-		./${capellaProductPath}
-			-port 8081 
-			-application org.polarsys.capella.test.run.application 
-			-data ${WORKSPACE}/runner >> ${WORKSPACE}/capella/runner.log
-	"""
+  return 
+		"./${capellaProductPath} " +
+			"-port 8081 " +
+			"-application org.polarsys.capella.test.run.application " +
+			"-data ${WORKSPACE}/runner >> ${WORKSPACE}/capella/runner.log"
 }
 
 private def getJunitCmdTemplate(String capellaProductPath) {
@@ -12,24 +11,24 @@ private def getJunitCmdTemplate(String capellaProductPath) {
   // extract the capella path, without the executable name
   def capellaPath = capellaProductPath.substring(0, capellaProductPath.lastIndexOf("/"))
   
-  return """
-    sleep 10 && 
-    java 
-      -Xms1024m -Xmx3500m -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -ea 
-      -Declipse.p2.data.area=@config.dir/../p2 
-      -Dfile.encoding=Cp1252 
-      -classpath ${capellaPath}/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.equinox.launcher.Main 
-      -os linux 
-      -ws gtk 
-      -arch x86_64 
-      -version 3 
-      -port 8081 
-      -testLoaderClass org.eclipse.jdt.internal.junit4.runner.JUnit4TestLoader 
-      -loaderpluginname org.eclipse.jdt.junit4.runtime 
-      -product org.polarsys.capella.rcp.product 
-      -testApplication org.polarsys.capella.core.platform.sirius.ui.perspective.id 
-      -configuration file:${capellaPath}/configuration 
-      -buildKey ${BUILD_KEY}"""
+  return 
+    "sleep 10 && " +
+    "java " +
+      "-Xms1024m -Xmx3500m -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -ea " +
+      "-Declipse.p2.data.area=@config.dir/../p2 " +
+      "-Dfile.encoding=Cp1252 " +
+      "-classpath ${capellaPath}/plugins/org.eclipse.equinox.launcher_*.jar org.eclipse.equinox.launcher.Main " +
+      "-os linux " +
+      "-ws gtk " +
+      "-arch x86_64 " +
+      "-version 3 " +
+      "-port 8081 " +
+      "-testLoaderClass org.eclipse.jdt.internal.junit4.runner.JUnit4TestLoader " +
+      "-loaderpluginname org.eclipse.jdt.junit4.runtime " +
+      "-product org.polarsys.capella.rcp.product " +
+      "-testApplication org.polarsys.capella.core.platform.sirius.ui.perspective.id " +
+      "-configuration file:${capellaPath}/configuration " +
+      "-buildKey ${BUILD_KEY}"
 }
 
 
@@ -44,10 +43,10 @@ private def getNONUIJunitCmd(String capellaProductPath) {
 def runUITests(String capellaProductPath, String suiteTitle, String testPluginName, List<String> testClassNames) {
   def runnerCmd = getRunnerCmd(capellaProductPath)
   def junitCmd = getUIJunitCmd(capellaProductPath)
-  def testClassNamesParam = testClassNames.join(" ")
+  def testClassNamesParam = testClassNames.join(' ')
   
   wrap([$class: 'Xvnc', takeScreenshot: false, useXauthority: true]) {
-    sh "${runnerCmd} -title ${suiteTitle} & ${junitCmd} - data ${WORKSPACE}/${suiteTitle} -testpluginname ${testPluginName} -classNames ${testClassNamesParam}"
+    sh "${runnerCmd} -title ${suiteTitle} & ${junitCmd} -data ${WORKSPACE}/${suiteTitle} -testpluginname ${testPluginName} -classNames ${testClassNamesParam}"
   }
   
 }
@@ -58,7 +57,7 @@ def runNONUITests(String capellaProductPath, String suiteTitle, String testPlugi
   def testClassNamesParam = testClassNames.join(" ")
   
   wrap([$class: 'Xvnc', takeScreenshot: false, useXauthority: true]) {
-  	sh "${runnerCmd} -title ${suiteTitle} & ${junitCmd} - data ${WORKSPACE}/${suiteTitle} -testpluginname ${testPluginName} -classNames ${testClassNamesParam}"
+  	sh "${runnerCmd} -title ${suiteTitle} & ${junitCmd} -data ${WORKSPACE}/${suiteTitle} -testpluginname ${testPluginName} -classNames ${testClassNamesParam}"
   }
   
 }
